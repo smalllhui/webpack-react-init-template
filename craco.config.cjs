da5c9c1aee6ba4999dec7b5cb2dff21cc956e630
smalllhui <smallhui2021@163.com>
2023-09-25 08:26:53 +0800

    feat: 完成less及全局less的配置

--- craco.config.cjs
+++ craco.config.cjs
@@ -1,8 +1,22 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-
+//配置less
+const CracoLessPlugin = require('craco-less')
+//配置全局less
+const cracoPluginStyleResourcesLoader = require('craco-plugin-stylus-resources-loader')
 // 具体配置见官网：https://craco.js.org/docs/
 const path = require('path')
 module.exports = {
+  // 插件配置
+  plugins: [
+    { plugin: CracoLessPlugin }, // 配置less
+    {
+      plugin: cracoPluginStyleResourcesLoader, //配置全局less
+      options: {
+        patterns: [path.join(__dirname, 'src/assets/styles/less/global.less')],
+        styleType: 'less',
+      },
+    },
+  ],
   // webpack 配置
   webpack: {
     // 配置别名
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import styles from '@/app.module.less'
 
 /**
  * @Description:App页面
  */
 const App: React.FC = () => {
-  return <div>App</div>
+  return <div className={styles.container}>App</div>
 }
 
 export default App
