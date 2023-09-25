8973cfb1eb57a24b8ca2c2532aa179dc6fee3ecf
smalllhui <smallhui2021@163.com>
2023-09-25 08:10:53 +0800

    feat: 完成@别名的配置

---
+++ craco.config.cjs
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+// 具体配置见官网：https://craco.js.org/docs/
+const path = require('path')
+module.exports = {
+  // webpack 配置
+  webpack: {
+    // 配置别名
+    alias: {
+      // 约定：使用 @ 表示 src 文件所在路径
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+}
